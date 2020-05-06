8fc18aae031012cb041b17ca4303972bb1191d92
Mateusz <mateusz.niescier@onet.pl>
2020-05-06 14:13:41 +0200

    Salsa20, RSA

--- src/ciphers.py
+++ src/ciphers.py
@@ -2,6 +2,9 @@
 from Crypto.Cipher import DES3, AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
+from Crypto.Cipher import Salsa20
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 def AES_encrypt(key: bytes, plaintext: str, mode: int, iv=None, nonce=None, counter=0):
     """
@@ -88,3 +91,32 @@ def DES3_decrypt(key: bytes, ciphertext: bytes, mode=DES3.MODE_ECB, iv=None, non
     else:
         return None
     return unpad(cipher.decrypt(ciphertext), DES3.block_size).decode('utf-8')
+
+def encrypt_Salsa20(plaintext, secret):
+    bytetext = bytes(plaintext, 'utf-8')
+    cipher = Salsa20.new(key=secret)
+    msg = cipher.nonce + cipher.encrypt(bytetext)
+    return msg
+
+def decrypt_Salsa20(msg, secret):
+    msg_nonce = msg[:8]
+    ciphertext = msg[8:]
+    cipher = Salsa20.new(key=secret, nonce=msg_nonce)
+    plaintext = cipher.decrypt(ciphertext)
+    return str(plaintext, 'utf-8')
+
+def get_key_pair():
+    private_key = RSA.generate(2048)
+    public_key = private_key.publickey()
+    return public_key, private_key
+
+def encrypt_RSA(plaintext, public_key):
+    bytetext = bytes(plaintext, 'utf-8')
+    cipher_rsa = PKCS1_OAEP.new(public_key)
+    encoded_data = cipher_rsa.encrypt(bytetext)
+    return encoded_data
+
+def decrypt_RSA(encoded_data, private_key):
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    bytetext = cipher_rsa.decrypt(encoded_data)
+    return str(bytetext, 'utf-8')
